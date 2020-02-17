585a77206a4b61422112e967fd8838656155320b
MarvellousWays <49512779+NikitaTerentjevs@users.noreply.github.com>
2020-02-17 18:35:15 +0200

    Update fifolifo.py

--- FiFoLiFo/fifolifo.py
+++ FiFoLiFo/fifolifo.py
@@ -1,8 +1,15 @@
-print("hello world")
+def printfifo(arr):             # fifo print()
+    for i in arr:
+        print(i)
 
-arr = []
+
+def printlifo(arr):             # lifo print()
+    for i in reversed(arr):
+        print(i)
+
+
 for i in range(10):
-    arr.append(i*i)
+    arr.insert(i, i*i)              # lifo push()
 
 arr.pop()           # last in first out
 
@@ -10,5 +17,11 @@ arr.reverse()   #
 arr.pop()       # first in first out
 arr.reverse()   #
 
-for i in arr:
-    print(i)
+list = []                           # fifo push(){
+for i in range(10,-1,-1):           #
+    list.append(i)                  #
+list.reverse()                      # }
+
+printfifo(list)
+print()
+printlifo(arr)
